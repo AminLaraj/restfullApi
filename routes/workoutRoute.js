6c19e097b9b757b060f74039ca3fb62d1daa72b3
AminLaraj <9014648@student.zadkine.nl>
2025-03-19 10:46:54 +0100

    opdracht 3

--- routes/workoutRoute.js
+++ routes/workoutRoute.js
@@ -1,26 +1,11 @@
 import express from "express";
-import Workout from "../models/Workout.js";
+import {
+  createWorkout,
+  getAllWorkouts,
+  getWorkout,
+} from "../controllers/workoutController.js";
 const router = express.Router();
-
-// alle workouts
-router.get("/", (req, res) => {
-  res.json("Welkom op de app");
-});
-
-// enkele workout
-router.get("/:id", (req, res) => {
-  res.json("Welkom op de app");
-});
-
-// workout toevoegen
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-  try {
-    const workout = await Workout.create({ title, reps, load });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
+router.get("/", getAllWorkouts);
+router.get("/:id", getWorkout);
+router.post("/", createWorkout);
 export default router;
